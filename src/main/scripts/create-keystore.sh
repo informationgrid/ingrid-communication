98da9d132b6707e1e8a590c461f158b9fb6ae355
mb <mb@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2007-06-11 10:05:53 +0000

    configure plugs for test system for generate keystores

--- src/main/scripts/create-keystore.sh
+++ src/main/scripts/create-keystore.sh
@@ -11,8 +11,45 @@ KEY_PASS=password
 STORE_PASS=password
 DNAME='CN=commonName, OU=organizationUnit, O=organization, L=location, S=state, C=country'
 
-servers=( ibus ibus2 )
-clients=( mb ms ak )
+servers=( torwald-ibus )
+clients=( 	torwald-iplug-management \
+			torwald-iplug-g2k-uok_by \
+			torwald-iplug-g2k-upb \
+			torwald-iplug-g2k-uok2_by \
+			torwald-iplug-g2k-ulidat \
+			torwald-iplug-g2k-ufordat \
+			torwald-iplug-fpn \
+			iplug-torwald-sns \
+			kug-iplug-csw-portalu \
+			kug-iplug-csw-sdi \
+			kug-iplug-csw-wsv \
+			kug-iplug-csw-wemove \
+			iplug-torwald-ecs-portalu \
+			iplug-torwald-ecs-ni \
+			torwald-iplug-udk-db_bw \
+			torwald-iplug-udk-db_hh \
+			torwald-iplug-udk-db_ni \
+			torwald-iplug-udk-db_rp \
+			torwald-iplug-udk-db_sl \
+			torwald-iplug-udk-db_uba \
+			torwald-iplug-udk-db_test \
+			torwald-iplug-udk-db_bw_addr \
+			torwald-iplug-udk-db_hh_addr \
+			torwald-iplug-udk-db_ni_addr \
+			torwald-iplug-udk-db_rp_addr \
+			torwald-iplug-udk-db_sl_addr \
+			torwald-iplug-udk-db_uba_addr \
+			torwald-iplug-udk-db_test_addr \
+			iplug-torwald-dsc \
+			iplug-se-search \
+			iplug-se-index \
+			opensearch \
+			portal \
+			torwald-iplug-jm \
+			torwald-iplug-tk \
+			torwald-iplug-ak \
+			torwald-iplug-ms \
+			torwald-iplug-mb )
 
 THIS_DIR=`dirname "$THIS"`
 
@@ -51,7 +88,7 @@ THIS_DIR=`dirname "$THIS"`
 
 			done
 
-		  rm  $THIS_DIR/$OUT/$client.cer
+#		  rm  $THIS_DIR/$OUT/$client.cer
 
 		
 	done
